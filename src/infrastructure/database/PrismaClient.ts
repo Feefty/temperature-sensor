d6ead6f2d59d57a34fed521b063244ca764c1796
rodolphe <poon.rodolphe@hotmail.fr>
2026-04-10 15:58:37 +0000

    Add routes

---
+++ src/infrastructure/database/PrismaClient.ts
@@ -0,0 +1,7 @@
+
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../../db/generated/client";
+const pool = new Pool({ connectionString: `${process.env.DATABASE_URL}` });
+const adapter = new PrismaPg(pool);
+export const prisma = new PrismaClient({ adapter });
